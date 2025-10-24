3d1ddb987fec15019fa547d05383cca698aeca1c
agathae <agatha.silva-youxlab@institutoyoux.org.br>
2025-10-24 16:48:19 -0300

    Arrumei o defeito do codigo do exercicio 03

--- exercicio03.py
+++ exercicio03.py
@@ -1,44 +1,43 @@
 continuar = 'S'
 aluno = []
 listadenotas = []
-listadeslunos =  []
+listadealunos = []
 i = 0
 while continuar != 'N':
-    aluno.clear() 
+    aluno.clear()
     nome = str(input('Digite o nome do aluno: '))
     aluno.append(nome)
     listadenotas.clear()
-
-    continuar_notas = 'S' 
+    continuar_notas = 'S'
     while continuar_notas != 'N':
         nota = float(input('Digite a nota do aluno: '))
         listadenotas.append(nota)
         continuar_notas = input('Deseja adicionar mais notas [S/N]?: ').upper()
+
     aluno.append(listadenotas)
-    listadeslunos.append(aluno.copy())
+    listadealunos.append(aluno.copy())
     continuar = input('Deseja adicionar mais alunos [S/N]?: ').upper()
 
 print(' REGISTRO DE ALUNOS')
-print(listadeslunos) 
+print(listadealunos) 
 
-while i < len(listadeslunos):
-        aluno_dado = listadeslunos[i]
+while i < len(listadealunos):
+        aluno_dado = listadealunos[i]
         nome = aluno_dado[0]
         notas = aluno_dado[1]
         
         media = 0
         if notas:
             media = sum(listadenotas) / len(listadenotas)
-        
-        print(f'{i, nome, media}')
+            print(f'{i, nome, media}')
         i += 1         
 consultar = 'S'
 while consultar == 'S':
         consulta = input("Digite o NÚMERO de índice do aluno para ver as notas: ")
         indice = int(consulta)
 
-        if 0 <= indice < len(listadeslunos):
-            aluno_dado = listadeslunos[indice]
+        if 0 <= indice < len(listadealunos):
+            aluno_dado = listadealunos[indice]
             nome = aluno_dado[0]
             notas = aluno_dado[1]
 
