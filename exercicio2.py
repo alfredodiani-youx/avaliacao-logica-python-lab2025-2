cc695d9463246a6fbe17f9d584140ee2a7e4ebc3
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-10-24 18:51:28 -0300

    corrigindo código

--- exercicio2.py
+++ exercicio2.py
@@ -6,6 +6,7 @@ for numero in range(5):
     mediaNumeros = sum(numerosInteiros) / len(numerosInteiros)
     menorInteiro = min(numerosInteiros)
     maiorInteiro = max(numerosInteiros)
+
 print('-'*30)
 print('         RESULTADO    ')
 print('-'*30)
@@ -15,7 +16,12 @@ print(f'    Média dos números: {mediaNumeros}.')
 print(f'    Lista em ordem crescente: {sorted(numerosInteiros)}.')
 numerosInteiros.sort(reverse=True)
 print(f'    Lista em ordem decrescente: {numerosInteiros}.')
-
+numeroposicao = int(input('>> Digite um número para buscar na lista: '))
+if numeroposicao in numerosInteiros:
+    posicao = numerosInteiros.index(numeroposicao)
+    print(f'  O número digitado está na lista! E está na {posicao}ª posição.')
+else:
+    print(f' O número digitado não está na lista!')
 
 
 
