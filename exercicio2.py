a0440674903559d4a426bcffed17098c45b4f5bd
luis2025git <luis.sousa-youxlab@institutoyoux.org.br>
2025-10-24 13:00:28 -0300

    exercicio 2 - LISTA E LACOS DE REPETICAO

---
+++ exercicio2.py
@@ -0,0 +1,24 @@
+numerosInteiros = []
+maiorInteiro = 0
+menorInteiro = 0
+for numero in range(5):
+    numerosInteiros.append(int(input('>> Digite um número inteiro: ')))
+    mediaNumeros = sum(numerosInteiros) / len(numerosInteiros)
+    menorInteiro = min(numerosInteiros)
+    maiorInteiro = max(numerosInteiros)
+print('-'*30)
+print('         RESULTADO    ')
+print('-'*30)
+print(f'    Maior número: {maiorInteiro}.')
+print(f'    Menor número: {menorInteiro}.')
+print(f'    Média dos números: {mediaNumeros}.')
+print(f'    Lista em ordem crescente: {sorted(numerosInteiros)}.')
+numerosInteiros.sort(reverse=True)
+print(f'    Lista em ordem decrescente: {numerosInteiros}.')
+
+
+
+
+
+
+
