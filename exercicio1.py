743d07f5ffe8cb0c302c1d8278571eb2b5c25e9d
GiovaneSilvaxss <giovane.silva-youxlab@institutoyoux.org.br>
2025-10-24 13:16:41 -0300

    ex1, nome,idade,nota media e se está na media,quantos anos falta para fazer 100 anos

---
+++ exercicio1.py
@@ -0,0 +1,16 @@
+nome = input("Digite o seu nome:")
+idade = int(input("Digite a sua idade:"))
+notamedia = float(input("Digite a sua nota:"))
+
+tempo = 100 - idade
+
+if notamedia >= 7.0:
+    situacao = ("Acima da média!")
+else:
+    situacao = ("Abaixo da média!")
+
+print("\n===== RESULTADO =====")
+print(f"Nome: {nome}")
+print(f"Idade: {idade} anos")
+print(f"Daqui a {tempo} anos, {nome} terá 100 anos!.")
+print(f"Sua nota média é {notamedia:1f}, o que está {situacao}.")
---
+++ exercicio2.py
@@ -0,0 +1,23 @@
+numeros = []
+
+for i in range(5):
+    n = int(input(f"Digite o {i+1}º número: "))
+    numeros.append(n)
+
+maior = max(numeros)
+menor = min(numeros)
+media = sum(numeros) / len(numeros)
+
+print("\nMaior:", maior)
+print("Menor:", menor)
+print("Média:", round(media, 2))
+
+print("Ordem crescente:", sorted(numeros))
+print("Ordem decrescente:", sorted(numeros, reverse=True))
+
+busca = int(input("\nDigite um número para verificar se está na lista: "))
+
+if busca in numeros:
+    print(f"O número {busca} está na posição {numeros.index(busca)}")
+else:
+    print("NÚMERO NÃO ENCONTRADO NA LISTA.")
