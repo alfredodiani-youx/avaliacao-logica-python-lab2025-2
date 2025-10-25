27042e8c734730375897a69e6ff1ebfc6c89469b
lais <laisseabra89@gemail.com>
2025-10-25 20:35:24 -0300

    exercicio 1, nome,média e idade

---
+++ exercicio1.py
@@ -0,0 +1,15 @@
+nome = input('Nome do aluno: ')
+idade = int(input('Sua idade: '))
+media = float(input('Digite a média: '))
+
+calculoIdade = idade - 100
+
+print('-' * 30)
+print(f'Sua idade daqui a 100 anos é:{calculoIdade}')
+print(f'')
+
+if media >= 7:
+    print(f'A média do aluno é maior que 7, então ele esta \033[32mAPROVADO\033[m')
+
+else:
+    print(f'A média do aluno é menor que 7, ele esta \033[31mREPROVADO\033[m')
