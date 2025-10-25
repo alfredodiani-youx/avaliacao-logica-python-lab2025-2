4d1fac47d11da23966bd31b8bd4cfcf8c80a0d94
daniella <daniella.flor-youxlab@institutoyoux.org.br>
2025-10-25 10:50:13 -0300

    exercicio 4- Fazendo uma lista de notas e o nome onde retorna em um dicionário que mostra o nome, media e situaçao de cada (3) alunos

---
+++ exercicio03.py
@@ -0,0 +1,33 @@
+resposta = 'S'
+alunos = []
+lista_nomes = []
+lista_notas = []
+
+while resposta != 'N':
+    lista_nomes.clear
+    nomes = str(input("Qual o seu nome?: "))
+    alunos.append(nomes)
+    lista_notas.clear()
+    resposta_nota = "S"
+    while resposta_nota == "S":
+        nota = float(input(f"Qual a nota de {nomes}?: "))
+        lista_notas.append(nota)
+        adicao_nota = str(input('Deseja adicionar mais notas [S/N?]: ')).upper()
+
+    alunos.append(lista_notas)
+    lista_nomes.append(alunos.copy())
+    pergunta = str(input('Deseja continuar [S/N]?: ')).upper()
+    
+print(" ")
+print(alunos)
+
+for pesquisa, dados in enumerate(alunos):
+    print(f"{pesquisa}{dados}{dados}")
+while True:
+    opcao = str(input("Qual alunos deseja ver? [Digite fim para finalizar o codígo]: "))
+    if opcao == "fim":
+        print("Finalizado")
+        break
+    opcao = int(opcao)
+    if opcao <= len(alunos) -1:
+        print(f"As notas de {alunos[opcao][0]} são {alunos[opcao][1]}")
