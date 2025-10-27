e5171cbd83c5d133e7a4d012015de4fb253c4b15
pedro-couto1-git <pedrocouto.profissional@gmail.com>
2025-10-26 21:54:44 -0300

    Exercicio 2- Lista e laços de repetição - sistema de analise de lista

---
+++ exercicio02.py
@@ -0,0 +1,23 @@
+numeros=[]
+soma=0
+maior=0
+menor=9999
+for c in range (0, 5):
+    numero=int(input('Digite um número'))
+    numeros.append(numero)
+    if maior<numero:
+        maior=numero
+    if menor>numero:
+        menor=numero
+    soma=soma+numero
+media=soma/5
+print(f'LISTA FEITA! o maior número inserido foi {maior} o menor foi {menor} e a media dos números foi {media}')
+print(f'Lista em ordem crescente {sorted(numeros)}')
+print(f'Lista em ordem decrescente {sorted(numeros, reverse=True)}')
+verificação= int(input('Insira um valor para verificarmos usa existencia na lista e sua posição'))
+if verificação in numeros:
+    for i, valor in enumerate(numeros):
+        if valor == verificação:
+            print(f'O número {verificação} está na posição {i}')
+else:
+    print(f'O número não esta na lista')
